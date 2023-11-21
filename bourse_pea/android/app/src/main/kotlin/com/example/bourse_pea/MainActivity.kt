bedb3aa5e9b877cd76a6e03c10ff5f0a5ea27999
Pierre160493 <pierre160493@gmail.com>
2023-11-21 12:05:33 +0100

    FirstCommit_Fonctionne

---
+++ bourse_pea/android/app/src/main/kotlin/com/example/bourse_pea/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bourse_pea
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bourse_pea/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://bgedhxfmdamqoosxqjqi.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJnZWRoeGZtZGFtcW9vc3hxanFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA1NTQ3MDEsImV4cCI6MjAxNjEzMDcwMX0.x222BhbcwCb-8mdCAhDvD55PqqlSu1SE4FwmDMh9Kfw',
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Countries',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _future = Supabase.instance.client
+      .from('countries')
+      .select<List<Map<String, dynamic>>>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: _future,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final countries = snapshot.data!;
+          return ListView.builder(
+            itemCount: countries.length,
+            itemBuilder: ((context, index) {
+              final country = countries[index];
+              return ListTile(
+                title: Text(country['name']),
+              );
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
